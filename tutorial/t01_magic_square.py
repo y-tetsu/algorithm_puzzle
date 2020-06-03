5422e09a69f4bca33e71d2d37aa255d324dfb257
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-06-03 11:50:02 +0900

    add file

--- tutorial/t01_magic_square.py
+++ tutorial/t01_magic_square.py
@@ -3,45 +3,54 @@
 
 import itertools
 
-N = 3
-cnt = 0
 
-for m in itertools.permutations([i + 1 for i in range(N*N)], N*N):
-    # initial
-    total = sum([m[i] for i in range(0, N*N, N)])
+class MagicSquare:
+    """magic square"""
+    def __init__(self, n):
+        self.n = n
+        self.count = 0
 
-    # horizontal
-    horizontal = True
-    for offset in range(N):
-        if total != sum([m[offset*N + i] for i in range(N)]):
-            horizontal = False
-            break
-    if not horizontal:
-        continue
+    def solve(self):
+        """solve"""
+        n = self.n
+        for pattern in itertools.permutations([i + 1 for i in range(n*n)], n*n):
+            # initial
+            total = sum([pattern[i] for i in range(0, n*n, n)])
 
-    # vertical
-    vertical = True
-    for offset in range(N):
-        if total != sum([m[offset + i] for i in range(0, N*N, N)]):
-            vertical = False
-            break
-    if not vertical:
-        continue
+            # horizontal
+            horizontal = True
+            for offset in range(n):
+                if total != sum([pattern[offset*n + i] for i in range(n)]):
+                    horizontal = False
+                    break
+            if not horizontal:
+                continue
 
-    # diagonal
-    diagonal = True
-    if total != sum([m[i*N + i] for i in range(N)]):
-        diagonal = False
-    if total != sum([m[(N-1-i)*N + i] for i in range(N)]):
-        diagonal = False
-    if not diagonal:
-        continue
+            # vertical
+            vertical = True
+            for offset in range(n):
+                if total != sum([pattern[offset + i] for i in range(0, n*n, n)]):
+                    vertical = False
+                    break
+            if not vertical:
+                continue
 
-    # result
-    for i in range(N):
-        print(m[i*N:(i+1)*N])
-    print()
-    cnt += 1
+            # diagonal
+            diagonal = True
+            if total != sum([pattern[i*n + i] for i in range(n)]):
+                diagonal = False
+            if total != sum([pattern[(n-1-i)*n + i] for i in range(n)]):
+                diagonal = False
+            if not diagonal:
+                continue
 
-# cnt
-print('cnt= =', cnt)
+            # result
+            for i in range(n):
+                print(pattern[i*n:(i+1)*n])
+            print()
+            self.count += 1
+
+        return self.count
+
+
+print('count= =', MagicSquare(3).solve())
--- tutorial/t02_the_n_queens_problem.py
+++ tutorial/t02_the_n_queens_problem.py
@@ -1,12 +1,11 @@
 """The n-Queens Problem
 """
 
-N = 4
-
 
 class nQueen:
     """The n-Queens Problem by backtracking"""
     def __init__(self, n):
+        self.n = n
         self.directions = [
             (-1, 1),  (0, 1),  (1, 1),
             (-1, 0),           (1, 0),
@@ -17,43 +16,46 @@ class nQueen:
 
     def backtracking(self, x=0):
         """backtracking"""
-        if x >= N:
-            for i in range(N):
-                print(self.board[N*i:N*(i+1)])
+        n = self.n
+        if x >= n:
+            for i in range(n):
+                print(self.board[n*i:n*(i+1)])
             print()
             return 1
         else:
-            cnt = 0
-            for y in [i for (i, v) in enumerate(self.control[x:N*N:N]) if v == 0]:
+            count = 0
+            for y in [i for (i, v) in enumerate(self.control[x:n*n:n]) if v == 0]:
                 self._put(x, y)
-                cnt += self.backtracking(x+1)
+                count += self.backtracking(x+1)
                 self._remove(x, y)
-            return cnt
+            return count
 
     def _put(self, x, y):
         """put queen"""
-        self.board[x+y*N] = 'Q'
+        self.board[x+y*self.n] = 'Q'
         self._update_control(x, y, 1)
 
     def _remove(self, x, y):
         """remove queen"""
-        self.board[x+y*N] = '_'
+        self.board[x+y*self.n] = '_'
         self._update_control(x, y, -1)
 
     def _update_control(self, x, y, delta):
         """update queen control"""
-        index = x + y * N
+        n = self.n
+        index = x + y * n
         self.control[index] += delta
         for dx, dy in self.directions:
             next_index, next_x, next_y = index, x, y
             while True:
                 next_x += dx
                 next_y += dy
-                if 0 <= next_x < N and 0 <= next_y < N:
-                    next_index += dx + dy * N
+                if 0 <= next_x < n and 0 <= next_y < n:
+                    next_index += dx + dy * n
                     self.control[next_index] += delta
                 else:
                     break
 
 
-print('cnt =', nQueen(N).backtracking())
+print('(N=4) count=', nQueen(4).backtracking(), '\n')
+print('(N=8) count=', nQueen(8).backtracking(), '\n')
